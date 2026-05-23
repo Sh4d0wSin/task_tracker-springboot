a06eebf2ca74a8f7d513b607e2e3ce90342b1c3a
Sh4d0wSin <s.wnarula@outlook.com>
2026-05-23 14:16:19 +0200

    test: add Mockito unit tests for updateTask and createTask in TaskService

--- src/test/java/com/shadowSin/task_tracker/service/TaskServiceTest.java
+++ src/test/java/com/shadowSin/task_tracker/service/TaskServiceTest.java
@@ -1,6 +1,9 @@
 package com.shadowSin.task_tracker.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.*;
@@ -10,8 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.shadowSin.task_tracker.exception.TaskNotFoundException;
 import com.shadowSin.task_tracker.model.Task;
 import com.shadowSin.task_tracker.repository.TaskRepository;
+
 
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
@@ -23,8 +28,8 @@ public class TaskServiceTest {
     TaskService taskService;
 
 
-    @Test
-    public void testGetAllTasks() {
+    @Test //happy case
+    public void testGetAllTasksHappy() {
         Task dummy1 = new Task();
         List<Task> tasks = List.of(dummy1);
 
@@ -36,9 +41,105 @@ public class TaskServiceTest {
 
         assertEquals(foundTasks, tasks);
 
-
-
-
     }
+
+    @Test //sad case
+    public void testGetTaskByIdSad() {
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+
+        assertThrows(TaskNotFoundException.class, () -> taskService.getTaskById(1L));
+    }
+
+    
+    @Test //happy case
+    public void testGetTaskByIdHappy() {
+
+        Task dummy1 = new Task();
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(dummy1));
+
+
+        assertEquals(dummy1, taskService.getTaskById(1L));
+    }
+
+
+    @Test //sad case
+    public void testDeleteTaskSad() {
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+
+        assertThrows(TaskNotFoundException.class, () -> taskService.deleteTask(1L));
+    }
+
+
+    @Test //happy case
+    public void testDeleteTaskHappy() {
+
+        Task dummy1 = new Task();
+
+        dummy1.setId(1L);
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(dummy1));
+
+        taskService.deleteTask(1L);
+
+        verify(taskRepository).deleteById(1L);
+
+
+
+
+        
+    }
+
+
+    @Test //happy case
+    public void testUpdateTaskByIdHappy() {
+
+        Task dummy1 = new Task();
+        Task updatedTask= new Task();
+
+        updatedTask.setCompleted(true);
+        updatedTask.setDescription("updated");
+        updatedTask.setTitle("Updated");
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(dummy1));
+        when(taskRepository.save(any(Task.class))).thenReturn(updatedTask);
+
+  
+
+        assertEquals(updatedTask, taskService.updateTask(updatedTask,1L));
+    }
+
+
+
+    @Test //sad case
+    public void testUpdateTaskSad() {
+        Task task = new Task();
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+
+        assertThrows(TaskNotFoundException.class, () -> taskService.updateTask(task,1L));
+    }
+
+
+    @Test //happy case
+    public void testCreateTask() {
+        Task task = new Task();
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+
+        assertEquals(task, taskService.createTask(task));
+    }
+
+
+
+
+
+
+
+    
     
 }
